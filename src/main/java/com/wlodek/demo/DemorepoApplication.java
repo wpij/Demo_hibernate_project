81c27a34db9678bb77f048f3b537efb017f32928
wpij <wpij@wp.pl>
2018-10-13 15:43:48 +0200

    first commit

---
+++ src/main/java/com/wlodek/demo/DemorepoApplication.java
@@ -0,0 +1,12 @@
+package com.wlodek.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemorepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemorepoApplication.class, args);
+	}
+}
